4617f2440cf74b998fe55db9786ed69817644aec
e-n-p <nickellam0@gmail.com>
2024-06-17 19:23:23 +0200

    adding map domain

---
+++ src/main/java/com/checkPoint/domains/maps/Map.java
@@ -0,0 +1,25 @@
+package com.checkPoint.domains.maps;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Map {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String map;
+
+    private int height;
+    private int width;
+}
---
+++ src/main/java/com/checkPoint/domains/maps/MapRepository.java
@@ -0,0 +1,6 @@
+package com.checkPoint.domains.maps;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MapRepository extends JpaRepository<Map, Long> {
+}
